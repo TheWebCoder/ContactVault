0d925f303c51e550aa538cefde370d2fa583a2d5
Bryan <127161500+TheWebCoder@users.noreply.github.com>
2023-11-15 15:16:23 -0800

    Created migrations, connected the database, and extended the default Identity User with two new fields

--- ContactVault/Data/ApplicationDbContext.cs
+++ ContactVault/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using ContactVault.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ContactVault.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
