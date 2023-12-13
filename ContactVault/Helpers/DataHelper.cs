abf07323a798a5661091aaa2adc961464570b142
Bryan <127161500+TheWebCoder@users.noreply.github.com>
2023-12-12 23:04:16 -0800

    Fixed navbar active link, fixed styling and contacts

---
+++ ContactVault/Helpers/DataHelper.cs
@@ -0,0 +1,17 @@
+﻿using ContactVault.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactVault.Helpers
+{
+    public static class DataHelper
+    {
+        public static async Task ManageDataAsync(IServiceProvider svcProvider)
+        {
+            //get an instance of the db application context
+            var dbContextsvc = svcProvider.GetRequiredService<ApplicationDbContext>();
+
+            //migration: this is equivalent to update-database
+            await dbContextsvc.Database.MigrateAsync();
+        }
+    }
+}
