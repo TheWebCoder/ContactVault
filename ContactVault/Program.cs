d4075856b96a89a88e738f03b154a38e7dd9d039
Bryan <127161500+TheWebCoder@users.noreply.github.com>
2023-12-12 23:27:53 -0800

    Added the connection string for deployment to heroku

--- ContactVault/Program.cs
+++ ContactVault/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using ContactVault.Helpers;
+using Npgsql;
 
 namespace ContactVault
 {
@@ -20,7 +21,22 @@ namespace ContactVault
             //var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
             //var connectionString = builder.Configuration.GetSection("pgSettings")["pgConnection"] ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            var connectionString = ConnectionHelper.GetConnectionString(builder.Configuration);
+            //var connectionString = ConnectionHelper.GetConnectionString(builder.Configuration);
+            
+
+            var databaseURI = new Uri("postgres://dkswxjmctuetey:93b82f4f7ae88c573485a7ddb296226dd29a3624c2fe0cc9a71e4b66e30bfa99@ec2-54-234-13-16.compute-1.amazonaws.com:5432/ddd5isuicvjtvq");
+            var userInfo = databaseURI.UserInfo.Split(':');
+
+            string connectionString = new NpgsqlConnectionStringBuilder()
+            {
+                Host = databaseURI.Host,
+                Port = databaseURI.Port,
+                Username = userInfo[0],
+                Password = userInfo[1],
+                Database = databaseURI.LocalPath.TrimStart('/'),
+                SslMode = SslMode.Prefer,
+                TrustServerCertificate = true
+            }.ToString();
 
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseNpgsql(connectionString));
